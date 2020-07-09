7d417adca585d2a23222e860bf990c152c9690b0
karim <afquir.karim@gmail.com>
2020-07-09 02:37:51 +0000

    dashboard

--- src/app/Dashboard/dashboard.component.ts
+++ src/app/Dashboard/dashboard.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import{Chart} from 'chart.js'
+
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +8,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  
+  chart=[];
   constructor() { }
 
   ngOnInit(): void {
+    this.chart = new Chart('canvas', {
+      type: 'line',
+      data: {
+        labels: ['Jan', 'Fev', 'Mar', 'Apr', 'May', 'Jun', 'Jul','Out','Sep','Oct','Nov','Dec'],
+        datasets: [
+          {
+            data: [0,7,4,5,3,9,8,8.5,12,7,5,3],
+            borderColor: 'red',
+            fill: true,
+            lineTension: 0.01,
+
+          },
+          {
+            data: [4,3,4,15,3,9,5,8.5,2,7,1,10],
+            borderColor: 'blue',
+            fill: true,
+            lineTension: 0.01,
+
+          },
+
+
+        ],
+
+      },
+      options: {
+        legend: {
+          display: false
+        },
+        scales: {
+          xAxes: [{
+            display: true,
+            gridLines: {
+            display: false
+            }
+          }],
+          yAxes: [{
+            display: false
+          }]
+        }
+      }
+    })
   }
 
 }
